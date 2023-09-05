347c5ba02eff8c36f5c1313e3d71641c992b922c
Vladislav <spanky@yandex.ru>
2023-09-05 13:08:53 +0300

    Fix gpsd.lua
    Added additional validation of the data coming from gspd, which could cause the application to crash

--- root/usr/share/gpoint/lib/gpsd.lua
+++ root/usr/share/gpoint/lib/gpsd.lua
@@ -81,7 +81,7 @@ function gpsd.getAllData(modemConfig)
     end
 
     gnssReq = json.parse(gnssReq)
-    if gnssReq.tpv[1] == nil or gnssReq.tpv[1].mode == 1 then
+    if gnssReq == nil and gnssReq.tpv == nil and gnssReq.tpv[1] == nil and gnssReq.tpv[1].mode == nil or gnssReq.tpv[1].mode == 1 then
         for _, i in pairs(GnssData.warning) do
             i[2] = "Gnss data not found"
         end
